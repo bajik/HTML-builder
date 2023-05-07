db081e3b3756716d3129b30ac90f709225c418c1
Babenko Yevhenii <eababenko@gmail.com>
2023-05-07 22:31:03 +0300

    feat: add solve task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const {
+  stdin: input,
+  stdout: output,
+} = require('process');
+const readline = require('readline');
+
+const readlineInterface = readline.createInterface({ input, output });
+
+output.write('Enter text. To exit, enter "exit" or press "Ctrl+c"\n');
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+readlineInterface.on('line', (inputText) => {
+  if (inputText === 'exit') {
+    output.write('\nEnd program');
+    writeStream.end();
+    process.exit();
+  } else {
+    writeStream.write(inputText + '\n');
+  }
+});
+
+
+readlineInterface.on('SIGINT', () => {
+  output.write('\nEnd program');
+  writeStream.end();
+  process.exit();
+})
