c94007aa5679e70300cf1d96ab0c1a82ca865a1d
Babenko Yevhenii <eababenko@gmail.com>
2023-05-08 21:36:42 +0300

    feat: solved task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const pathNewFolder = path.join(__dirname, 'project-dist');
 const pathNewAssets = path.join(pathNewFolder, 'assets');
+const pathAssets = path.join(__dirname, 'assets');
 const pathComponents = path.join(__dirname, 'components');
 const templateFile = path.join(__dirname, 'template.html');
+const pathStyle = path.join(__dirname, 'styles');
 
 async function deleteAllFilesAndFolders(directoryPath) {
   try {
@@ -75,12 +77,53 @@ async function readTemplateFile() {
   });
 }
 
+async function copyFolder(currentFolder, newFolder) {
+  const files = await fs.promises.readdir(currentFolder);
+  for (const file of files) {
+    const filePath = path.join(currentFolder, file);
+    const stats = await fs.promises.stat(filePath);
+    // console.log(file, stats.isFile());
+    if (stats.isFile()) {
+      fs.copyFile(path.join(currentFolder, file), path.join(newFolder, file), (errCopy) => {
+        if (errCopy) throw errCopy;
+      });
+    } else {
+      const oFolder = path.join(currentFolder, file);
+      const nFolder = path.join(newFolder, file);
+      await createFolder(nFolder);
+      await copyFolder(oFolder, nFolder)
+    }
+  }
+}
+
+async function mergeCSS() {
+  const files = await fs.promises.readdir(pathStyle, { withFileTypes: true });
+  const outputStream = fs.createWriteStream(path.join(pathNewFolder, 'style.css'));
+
+  for (let i = 0; i < files.length; i += 1) {
+    if (files[i].isDirectory()) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    const filePath = path.join(pathStyle, files[i].name);
+    const fileParse = path.parse(filePath);
+    if (fileParse.ext !== '.css') {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(outputStream);
+  }
+}
+
 async function build() {
   try {
     await deleteAllFilesAndFolders(pathNewFolder);
     await createFolder(pathNewFolder);
     await readTemplateFile();
     await createFolder(pathNewAssets);
+    await copyFolder(pathAssets, pathNewAssets);
+    await mergeCSS();
     // ...
   } catch (err) {
     console.error(err);
